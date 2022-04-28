5fa2082cea24b533a45e909d291b78636fd51f73
starwalker00 <aaronwalkerup@gmail.com>
2022-04-28 22:36:40 +0200

    use data in list

--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -1,3 +1,39 @@
-import List from 'src/components/List'
+//@ts-nocheck
+import { Layout, Navbar } from 'src/components/Layout'
 
-export default List
+const chainjsUrl = 'https://raw.githubusercontent.com/starwalker00/chain-rpc-list/main/data/rpcList.json';
+
+function List({ rpcs }) {
+    return (
+        <ul>
+            {rpcs.map((rpc) => (
+                <li>{rpc.networkName}</li>
+            ))}
+        </ul>
+    )
+}
+
+List.getLayout = function getLayout(page: ReactElement) {
+    return (
+        <Layout>
+            <Navbar />
+            {page}
+        </Layout>
+    )
+}
+
+export async function getStaticProps() {
+
+    // fetch temporarily from github, but later, make it an external repo ?
+    const res = await fetch(chainjsUrl);
+    const rpcs = await res.json();
+
+    return {
+        props: {
+            rpcs,
+        },
+    }
+}
+
+
+export default List;
