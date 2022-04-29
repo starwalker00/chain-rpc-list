57dfacc4c6afa1b869c890b753af228653560996
starwalker00 <aaronwalkerup@gmail.com>
2022-04-29 14:15:35 +0200

    refactor

--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -1,6 +1,6 @@
 //@ts-nocheck
 import { Layout, Navbar } from 'src/components/Layout'
-import { Table, Thead, Tbody, Tr, Th, Td, chakra } from '@chakra-ui/react'
+import { Container, Box, TableContainer, Table, Thead, Tbody, Tr, Th, Td, chakra } from '@chakra-ui/react'
 import { TriangleDownIcon, TriangleUpIcon } from '@chakra-ui/icons'
 import { useTable, useGlobalFilter, useAsyncDebounce, useExpanded, useSortBy } from 'react-table'
 import { useState, useMemo, useCallback } from 'react'
@@ -8,7 +8,7 @@ import 'regenerator-runtime/runtime'
 
 const chainjsUrl = 'https://raw.githubusercontent.com/starwalker00/chain-rpc-list/main/data/rpcList.json';
 
-// Define a default UI for filtering
+// Define a default UI for global filtering
 function GlobalFilter({
     preGlobalFilteredRows,
     globalFilter,
@@ -40,26 +40,19 @@ function GlobalFilter({
 }
 
 function List({ rpcs }) {
-    // return (
-    //     <ul>
-    //         {rpcs.map((rpc) => (
-    //             <li>{rpc.networkName}</li>
-    //         ))}
-    //     </ul>
-    // )
     const columns = useMemo(
         () => [
             {
                 // Make an expander cell
                 Header: () => null, // No header
-                id: 'expander', // It needs an ID
+                id: 'expander', // It needs an 
                 Cell: ({ row }) => (
                     // Use Cell to render an expander for each row.
                     // We can use the getToggleRowExpandedProps prop-getter
                     // to build the expander.
-                    <span {...row.getToggleRowExpandedProps()}>
+                    <chakra.span maxWidth="2px"  {...row.getToggleRowExpandedProps()} >
                         {row.isExpanded ? '👇' : '👉'}
-                    </span>
+                    </chakra.span >
                 ),
             },
             {
@@ -101,64 +94,66 @@ function List({ rpcs }) {
         setGlobalFilter } =
         useTable({ columns, data }, useGlobalFilter, useSortBy, useExpanded);
     return (
-        <>
-            <Table {...getTableProps()}>
-                <Thead>
-                    {headerGroups.map((headerGroup) => (
-                        <Tr {...headerGroup.getHeaderGroupProps()}>
-                            {headerGroup.headers.map((column) => (
-                                <Th
-                                    {...column.getHeaderProps(column.getSortByToggleProps())}
-                                    isNumeric={column.isNumeric}
-                                >
-                                    {column.render('Header')}
-                                    <chakra.span pl='4'>
-                                        {column.isSorted ? (
-                                            column.isSortedDesc ? (
-                                                <TriangleDownIcon aria-label='sorted descending' />
-                                            ) : (
-                                                <TriangleUpIcon aria-label='sorted ascending' />
-                                            )
-                                        ) : null}
-                                    </chakra.span>
-                                </Th>
-                            ))}
-                        </Tr>
-                    ))}
-                    <GlobalFilter
-                        preGlobalFilteredRows={preGlobalFilteredRows}
-                        globalFilter={state.globalFilter}
-                        setGlobalFilter={setGlobalFilter}
-                    />
-                </Thead>
-                <Tbody {...getTableBodyProps()}>
-                    {rows.map((row, idx) => {
-                        prepareRow(row)
-                        return (
-                            <>
-                                <Tr key={idx} {...row.getRowProps()}>
-                                    {row.cells.map((cell) => (
-                                        <Td {...cell.getCellProps()} isNumeric={cell.column.isNumeric}>
-                                            {cell.render('Cell')}
-                                        </Td>
-                                    ))}
-                                </Tr>
-                                {row.isExpanded ? (
-                                    <Tr key={idx}>
-                                        <Td colSpan={visibleColumns.length}>
-                                            {renderRowSubComponent({ row })}
-                                        </Td>
+        <Container maxW='container.xl'>
+            <TableContainer>
+                <Table {...getTableProps()} variant='striped' colorScheme='teal'>
+                    <Thead>
+                        {headerGroups.map((headerGroup) => (
+                            <Tr {...headerGroup.getHeaderGroupProps()}>
+                                {headerGroup.headers.map((column) => (
+                                    <Th
+                                        {...column.getHeaderProps(column.getSortByToggleProps())}
+                                        isNumeric={column.isNumeric}
+                                    >
+                                        {column.render('Header')}
+                                        <chakra.span pl='4'>
+                                            {column.isSorted ? (
+                                                column.isSortedDesc ? (
+                                                    <TriangleDownIcon aria-label='sorted descending' />
+                                                ) : (
+                                                    <TriangleUpIcon aria-label='sorted ascending' />
+                                                )
+                                            ) : null}
+                                        </chakra.span>
+                                    </Th>
+                                ))}
+                            </Tr>
+                        ))}
+                        <GlobalFilter
+                            preGlobalFilteredRows={preGlobalFilteredRows}
+                            globalFilter={state.globalFilter}
+                            setGlobalFilter={setGlobalFilter}
+                        />
+                    </Thead>
+                    <Tbody {...getTableBodyProps()}>
+                        {rows.map((row, idx) => {
+                            prepareRow(row)
+                            return (
+                                <>
+                                    <Tr {...row.getRowProps()}>
+                                        {row.cells.map((cell) => (
+                                            <Td {...cell.getCellProps()} isNumeric={cell.column.isNumeric}>
+                                                {cell.render('Cell')}
+                                            </Td>
+                                        ))}
                                     </Tr>
-                                ) : null}
-                            </>
-                        )
-                    })}
-                </Tbody>
-            </Table>
+                                    {row.isExpanded ? (
+                                        <Tr>
+                                            <Td colSpan={visibleColumns.length}>
+                                                {renderRowSubComponent({ row })}
+                                            </Td>
+                                        </Tr>
+                                    ) : null}
+                                </>
+                            )
+                        })}
+                    </Tbody>
+                </Table>
+            </TableContainer>
             <pre>
                 <code>{JSON.stringify({ expanded: state.expanded }, null, 2)}</code>
             </pre>
-        </>
+        </Container>
     )
 }
 
@@ -173,7 +168,7 @@ List.getLayout = function getLayout(page: ReactElement) {
 
 export async function getStaticProps() {
 
-    // fetch temporarily from github, but later, make it an external repo ?
+    // fetch temporarily from github, but later, make it an external repo or internal resource or API ?
     const res = await fetch(chainjsUrl);
     const rpcs = await res.json();
 
